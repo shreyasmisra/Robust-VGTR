1dcab774b14cf90bc750bd6ad43233663a1eabb5
Ubuntu <ubuntu@ip-172-31-36-26.us-east-2.compute.internal>
2022-05-02 04:48:31 +0000

    running baseline

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@
 
 #python main.py --gpu 0 --savepath store/logs_co_attn_encoder/ --dataset refcoco+ 
 #python main.py --gpu 0 --savepath store/logs_co_attn_encoder/resume_26/ --resume store/logs_co_attn_encoder/resume_17/model/model_refcoco_batch_48/model_refcoco_checkpoint.pth.tar --data_perc 0.45
-#python main.py --gpu 0 --savepath store/baseline/resume --resume store/baseline/model/model_refcoco_batch_48/model_refcoco_checkpoint.pth.tar --data_perc 0.3
+python main.py --gpu 0 --savepath store/baseline/resume --resume store/baseline/model/model_refcoco_batch_48/model_refcoco_best.pth.tar --data_perc 0.3
 
-python main.py --gpu 0 --test --dataset refcoco --pretrain "/home/ubuntu/VGTR/store/logs_co_attn_encoder/resume_17/model/model_refcoco_batch_48/model_refcoco_best.pth.tar" --data_perc 0.99 --save_data "/home/ubuntu/VGTR/store/logs_co_attn_encoder/output/" --split testA
+#python main.py --gpu 0 --test --dataset refcoco --pretrain "/home/ubuntu/VGTR/store/logs_co_attn_encoder/resume_17/model/model_refcoco_batch_48/model_refcoco_best.pth.tar" --data_perc 0.99 --save_data "/home/ubuntu/VGTR/store/logs_co_attn_encoder/output/" --split testA
